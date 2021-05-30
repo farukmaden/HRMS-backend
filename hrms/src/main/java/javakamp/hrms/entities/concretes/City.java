a9d8757f63ca432f12025aa3c8eba3f8b7ffc63c
farukmaden <77931457+farukmaden@users.noreply.github.com>
2021-05-30 18:00:45 +0300

    Create City.java

---
+++ hrms/src/main/java/javakamp/hrms/entities/concretes/City.java
@@ -0,0 +1,37 @@
+package javakamp.hrms.entities.concretes;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cities")
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","job_advertisements"})
+public class City {
+	
+	@Id
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "city_name")
+	private String cityName;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "city")
+	private List<JobAdvertisement> jobAdvertisement;
+
+}
