28b885ba3bbf706dae01efebee4e269a9caf1d1e
farukmaden <77931457+farukmaden@users.noreply.github.com>
2021-05-30 18:00:43 +0300

    Create Employer.java

---
+++ hrms/src/main/java/javakamp/hrms/entities/concretes/Employer.java
@@ -0,0 +1,43 @@
+package javakamp.hrms.entities.concretes;
+
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = false)
+@Data
+@Entity
+@Table(name = "employers")
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","job_advertisements"})
+public class Employer extends User{
+
+	@Column(name="company_name")
+	private String companyName;
+	
+	@Column(name="web_address")
+	private String webAddress;
+	
+	@Column(name="phone_number")
+	private String phoneNumber;
+	
+	@Column(name="is_activated")
+	private boolean isActivated;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "employer")
+	private List<JobAdvertisement> jobAdvertisements;
+}
